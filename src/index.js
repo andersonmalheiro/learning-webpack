6ddf5ed1a62a66e87e2a14e2c63708cebbcfbf76
Anderson Malhero de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2021-04-18 15:50:11 -0300

    Adding output management

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import _ from 'lodash';
 import './assets/styles/style.css';
 import WebpackIcon from './assets/images/icon.png';
 
+import printMe from './print';
+
 function component() {
   const container = document.createElement('div');
   container.classList.add('container');
@@ -14,8 +16,13 @@ function component() {
   logo.src = WebpackIcon;
   logo.classList.add('logo');
 
+  const btn = document.createElement('button');
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
   container.appendChild(logo);
   container.appendChild(title);
+  container.appendChild(btn);
 
   return container;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack demo',
+    }),
+  ],
   mode: 'development',
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
