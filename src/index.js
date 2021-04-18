42dce2a323861497a0d349aaf6b80a395afa8902
Anderson Malhero de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2021-04-18 15:08:44 -0300

    Updating folder structure

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import _ from 'lodash';
-import './style.css';
+import './assets/styles/style.css';
+import WebpackIcon from './assets/images/icon.png';
 
 function component() {
-  const element = document.createElement('div');
+  const container = document.createElement('div');
+  container.classList.add('container');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+  const title = document.createElement('h1');
+  title.innerText = _.join(['Hello', 'webpack'], ' ');
+  title.classList.add('title');
 
-  return element;
+  const logo = new Image();
+  logo.src = WebpackIcon;
+  logo.classList.add('logo');
+
+  container.appendChild(logo);
+  container.appendChild(title);
+
+  return container;
 }
 
 document.body.appendChild(component());
